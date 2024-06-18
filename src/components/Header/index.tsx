eb907d8c689ac302b151e152fdaec1633b038778
Maycon Santos <maycon.santos@db1.com.br>
2024-06-17 22:49:34 -0300

    feat: componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 
+import osbLogo from "../assets/osb-logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={osbLogo} alt="OSB Logo" />
       <strong>Observatório Social do Brasil</strong>
     </header>
   );
